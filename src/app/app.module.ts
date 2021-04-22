61581243bbcfa959d19e225ff81ebbba1c96473c
Sangduen W <sangduen.w@kbtg.tech>
2021-04-22 12:06:03 +0700

    Add new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { XyzComponent } from './login/xyz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    XyzComponent
   ],
   imports: [
     BrowserModule,
